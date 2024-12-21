10bc5c65f6925e358ecedb877e470dd48ef2a7e8
Anton Yip <anton_yip@hotmail.com>
2024-12-21 12:42:07 +0800

    full build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-VERSION=1.0.0.$(date +%s)
+VERSION=1.1.0.$(date +%s)
 git pull
 docker build -t antonyip/covalent-light-client:$VERSION -f Dockerfile.lc .
 docker push antonyip/covalent-light-client:$VERSION
+docker build -t antonyip/covalent-light-client:latest -f Dockerfile.lc .
+docker push antonyip/covalent-light-client:latest
 
